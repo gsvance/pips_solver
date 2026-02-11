ce6f7051c314ffea8fe8972a4a20f95872cfd0ab
Gregory Vance <13666175+gsvance@users.noreply.github.com>
2026-02-10 21:46:27 -0500

    Add a test for parsing board layout strings

--- tests/test_space.py
+++ tests/test_space.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from basics import Space, TOPMOST_ROW, LEFTMOST_COLUMN
+from basics import Space, TOPMOST_ROW, LEFTMOST_COLUMN, parse_board_layout
 
 
 class TestSpace(unittest.TestCase):
@@ -28,3 +28,18 @@ class TestSpace(unittest.TestCase):
         self.assertEqual(space.shift_by(delta_c=-1), Space(5, 7))
         self.assertEqual(space.shift_by(delta_r=0, delta_c=0), space)
         self.assertEqual(space.shift_by(delta_r=-4, delta_c=2), Space(1, 10))
+
+    def test_parse_board_layout(self):
+        board_layout_string = '\n\n AAB\n CDD#\n'
+        expected_spaces = {
+            Space(TOPMOST_ROW + 0, LEFTMOST_COLUMN + 0): 'A',
+            Space(TOPMOST_ROW + 0, LEFTMOST_COLUMN + 1): 'A',
+            Space(TOPMOST_ROW + 0, LEFTMOST_COLUMN + 2): 'B',
+            Space(TOPMOST_ROW + 1, LEFTMOST_COLUMN + 0): 'C',
+            Space(TOPMOST_ROW + 1, LEFTMOST_COLUMN + 1): 'D',
+            Space(TOPMOST_ROW + 1, LEFTMOST_COLUMN + 2): 'D',
+            Space(TOPMOST_ROW + 1, LEFTMOST_COLUMN + 3): '#',
+        }
+        self.assertDictEqual(
+            parse_board_layout(board_layout_string), expected_spaces,
+        )
